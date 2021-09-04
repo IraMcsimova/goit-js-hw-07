30ab2b1c942182f3e080ae9a04c4272419411905
IraMcsimova <ilisay777@gmail.com>
2021-09-04 14:16:15 +0300

    task-02

---
+++ tasks.js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((elem) => {
+  const item = document.createElement("li");
+
+  item.textContent = elem;
+
+  ingredientsList.appendChild(item);
+});
+console.log(ingredientsList);
