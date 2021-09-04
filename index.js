0b4b7bc74f003ac206517ef2a3bc3c78eeac2eac
IraMcsimova <ilisay777@gmail.com>
2021-09-04 21:33:13 +0300

    task-05-03-corrections

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 // import "./js/task-02.js";
 // import "./js/task-03.js";
 // import "./js/task-04.js";
-import "./js/task-05.js";
+// import "./js/task-05.js";
+import "./js/task-06.js";
--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,12 @@
 const textInput = document.querySelector("#name-input");
-console.log(textInput);
-const textOutput = document.querySelector("#name-output");
-console.log(textOutput);
-textInput.addEventListener("input", (event) => {
-  //   event.preventDefault();
-  console.log(event.target.value);
 
-  if (event.target.value != 0) {
-    textOutput.textContent = `${event.target.value}`;
+const textOutput = document.querySelector("#name-output");
+
+textInput.addEventListener("input", updateGreet);
+function updateGreet(event) {
+  if (event.target.value != "") {
+    textOutput.textContent = event.target.value;
   } else {
-    textOutput.textContent;
+    textOutput.textContent = "незнакомец";
   }
-});
+}
